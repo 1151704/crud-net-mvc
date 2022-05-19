0d509aac385125bac9ce8957704b19361085d33b
OMAR RAMON <omarmontes.879@gmail.com>
2022-05-18 20:34:01 -0500

    Creación de Crud

--- TestCrud/Controllers/HomeController.cs
+++ TestCrud/Controllers/HomeController.cs
@@ -15,14 +15,7 @@ namespace TestCrud.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Administración de empleados.";
 
             return View();
         }
---
+++ TestCrud/Controllers/empleadosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestCrud;
+
+namespace TestCrud.Controllers
+{
+    public class empleadosController : Controller
+    {
+        private entities db = new entities();
+
+        // GET: empleados
+        public ActionResult Index()
+        {
+            return View(db.tblEmpleados.ToList());
+        }
+
+        // GET: empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmpleados tblEmpleados = db.tblEmpleados.Find(id);
+            if (tblEmpleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmpleados);
+        }
+
+        // GET: empleados/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: empleados/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombres,Apellidos")] tblEmpleados tblEmpleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblEmpleados.Add(tblEmpleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblEmpleados);
+        }
+
+        // GET: empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmpleados tblEmpleados = db.tblEmpleados.Find(id);
+            if (tblEmpleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmpleados);
+        }
+
+        // POST: empleados/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombres,Apellidos")] tblEmpleados tblEmpleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblEmpleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblEmpleados);
+        }
+
+        // GET: empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmpleados tblEmpleados = db.tblEmpleados.Find(id);
+            if (tblEmpleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmpleados);
+        }
+
+        // POST: empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblEmpleados tblEmpleados = db.tblEmpleados.Find(id);
+            db.tblEmpleados.Remove(tblEmpleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
